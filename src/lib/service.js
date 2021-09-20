10dc6cdbcd92e65596f406ba5051ef6e00ff24b0
elifnurkarakoc <elifnurkarakoc@gmail.com>
2021-09-20 17:05:06 +0300

    gitignore file added

--- src/lib/service.js
+++ src/lib/service.js
@@ -1,12 +1,15 @@
 import axios from "axios";
 
+/**
+ * Returns the user Object
+ * @param {number} user_id
+ * @returns {Object}
+ */
 async function getData(user_id){
 
     try{
         const {data:user} = await axios.get(`https://jsonplaceholder.typicode.com/users/${user_id}`);
         const {data:posts} =await axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${user_id}`);
-        // console.log(user);
-        // console.log('posts', posts);
         user.posts =posts
         return user;
     } 
@@ -14,7 +17,6 @@ async function getData(user_id){
         console.log(error.message);
     }
     
-
 };
 
 export default getData;
